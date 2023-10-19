af36d915645836bf78aecdda174d3321dd372075
Teenymario <wmfourie001@outlook.com>
2023-10-19 22:01:53 +1300

    Finalized the model for the basic engine and added in rotations for the basic engine. Very clearly let the game know that the basic engine is not a solid block :)

--- src/main/java/teenymario123/extuniverse/objects/blocks/blockBase.java
+++ src/main/java/teenymario123/extuniverse/objects/blocks/blockBase.java
@@ -1,12 +1,13 @@
 package teenymario123.extuniverse.objects.blocks;
 
 import net.minecraft.block.*;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.entity.EntityLivingBase;
 import teenymario123.extuniverse.util.interfaces.*;
 import net.minecraft.block.material.*;
 import teenymario123.extuniverse.init.*;
 import net.minecraft.item.*;
-import net.minecraft.client.renderer.block.model.*;
-import net.minecraftforge.client.model.*;
 import net.minecraftforge.fml.relauncher.*;
 import net.minecraft.block.state.*;
 import net.minecraft.world.*;
@@ -14,30 +15,74 @@ import net.minecraft.util.math.*;
 import net.minecraft.util.*;
 import teenymario123.extuniverse.*;
 
-public class blockBase extends Block implements IHasModel {
+@SuppressWarnings("deprecation")
+public class blockBase extends BlockDirectional implements IHasModel {
 
     public blockBase(String name, Material material) {
         super(material);
+        this.fullBlock = false;
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
         setRegistryName(name);
         setTranslationKey(name);
         setCreativeTab(main.ENGINES_TAB);
         blockInit.BLOCKS.add(this);
         itemInit.ITEMS.add(new ItemBlock(this).setRegistryName(name));
     }
-    
+
+    //Rotations
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] {FACING});
+    }
+
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.byIndex(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
+    }
+
+    @Override
+    public IBlockState withRotation(IBlockState state, Rotation rot) {
+        return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState().withProperty(FACING, facing);
+    }
+    //Rotations
+
+    //Block rendering
+    @Override
     @SideOnly(Side.CLIENT)
     public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
+        return true;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
         return false;
     }
-    
-    public boolean isBlockNormalCube(IBlockState blockState) {
-        return false;
-    }
-    
+
+    @Override
     public boolean isOpaqueCube(IBlockState blockState) {
         return false;
     }
 
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    //Block rendering
+
+    //Model registration
     @Override
     public void registerModels() {
         main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
